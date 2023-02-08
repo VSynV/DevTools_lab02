5edd78cecd748dae94fc85224123ff437e004793
VladDudkin <vuper4321@gmail.com>
2023-02-08 16:57:47 +0400

    add

---
+++ NewProj/NewProj/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Wintellect.PowerCollections;
+Wintellect.PowerCollections.Stack < string > S = new Wintellect.PowerCollections.Stack<string>(10);
+S.Push("Hello");
+Console.WriteLine(S.Pop());
