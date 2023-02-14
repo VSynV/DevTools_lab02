226d8cfb96cf9bb907dcdc26e9a3a2d8370b4120
VladDudkin <vuper4321@gmail.com>
2023-02-14 18:38:29 +0400

    add

--- PowerCollections/build_and_publish_nuget.cmd
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -1,4 +1,3 @@
 dotnet pack
-cd PowerCollections/bin/Debug
-dotnet nuget push "*.nupkg" -k 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 -s https://nuget.pkg.github.com/VSynV/index.json --skip-duplicate
+dotnet nuget push "PowerCollections\bin\Debug\*.nupkg" -k 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 -s https://nuget.pkg.github.com/VSynV/index.json --skip-duplicate
 pause
